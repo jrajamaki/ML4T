f21662d3708d8da8412727357d76900ce2094057
jrajamaki <jrajamaki@users.noreply.github.com>
2017-03-29 13:05:16 +0800

    Basic implementation of marketsim.py

--- mc2_p1/marketsim.py
+++ mc2_p1/marketsim.py
@@ -3,42 +3,64 @@
 import pandas as pd
 import numpy as np
 import datetime as dt
-import os
+import math
 from util import get_data, plot_data
-
-
-def read_trades_from_csv(orders_file):
-
-    return orders
 
 
 def compute_portvals(orders_file="./orders/orders.csv", start_val=1000000):
     # this is the function the autograder will call to test your code
     # TODO: Your code here
 
-    # Step 0: Get data
-    # Date, Symbol, Order(Buy/Sell), #Shares
-    orders = read_trades_from_csv(orders_file)
+    # Get data
+    # FORMAT: Date, Symbol, Order(Buy/Sell), #Shares
+    orders = pd.read_csv(orders_file, index_col=[0])
+    orders.sort_index()
 
-    # Determine start and end date from the 'orders'
+    # Determine stock symbols, start and end date from the 'orders'
+    ls_symbols = list(set(orders['Symbol'].values))
+    start_date = orders.index[0]
+    end_date = orders.index[-1]
+    dates = pd.date_range(start_date, end_date)
 
-    # Read prices to 'prices' dataframe
-    # Use adjusted close price
-    # Add cash column to prices
+    # Read prices to 'prices' dataframe, use adjusted close price
+    # Automatically adds SPY to ensure correct market dates
+    prices = get_data(ls_symbols, dates)
+    # SPY has to dropped if not in traded stocks
+    if 'SPY' not in ls_symbols:
+        prices.drop('SPY', axis=1, inplace=True)
+
+    # Add cash column to prices and set 1
+    prices = prices.assign(Cash=np.ones(len(prices)))
 
     # Create 'trades' dataframe by copying 'prices' dataframe
     # Fill with zeros, then populate with trade amounts, needs for-loop
-    # Update cash column in trades dataframe
+    trades = prices.copy() * 0
+    for order in orders.iterrows():
+        multiplier = 1
+        if order[1]['Order'] == 'SELL':
+            multiplier = -1
+        date = order[0]
+        stock = order[1]['Symbol']
+        amount = order[1]['Shares']
+        trades.ix[date, stock] += multiplier * amount
+
+        # Update cash column in trades dataframe
+        trades.ix[date, ['Cash']] += -1 * multiplier * amount * prices.ix[date, stock]
 
     # Create 'holdings' dataframe by copying trades dataframe
     # Initialize with zeros, except cash of day 1 with starting cash
     # Then update the first row
     # Upcoming dates are previous date and that day's trade
+    holdings = prices.copy() * 0
+    holdings.ix[0] = trades.ix[0]
+    holdings.ix[0, 'Cash'] += start_val
+    holdings[1:] += trades[1:]
+    holdings = holdings.cumsum()
 
     # Create 'values' dataframe by multiplying 'prices' and 'holdings'
-
+    values = holdings * prices
     # Calculate daily portfolio value by summing 'values' dataframe's columns
-    # sum(axis=1)
+    port_val = values.sum(axis=1).to_frame()
 
     # Secret: on June 15th, 2011 ignore all orders
 
@@ -46,16 +68,23 @@ def compute_portvals(orders_file="./orders/orders.csv", start_val=1000000):
     # How much invested in market / liquidation value of account
     # leverage = (sum(abs(all stock positions))) / (sum(all stock positions) + cash)
     # Constraint: Leverage cannot exceed 1.5, if so reject the trade
+    return port_val
 
-    ###
-    # In the template, instead of computing the value of the portfolio, we just
-    # read in the value of IBM over 6 months
-    start_date = dt.datetime(2008, 1, 1)
-    end_date = dt.datetime(2008, 6, 1)
-    portvals = get_data(['IBM'], pd.date_range(start_date, end_date))
-    portvals = portvals[['IBM']]  # remove SPY
 
-    return portvals
+def compute_portfolio_stats(port_val,
+                            rfr=0.0, sf=252.0):
+
+    # Calculate daily returns
+    daily_returns = port_val / port_val.shift(1) - 1
+    daily_returns = daily_returns[1:]
+
+    # Get portfolio statistics (note: std_daily_ret = volatility)
+    cr = port_val.ix[-1] / port_val.ix[0] - 1
+    adr = daily_returns.mean()
+    sddr = daily_returns.std()
+    sr = math.sqrt(sf) * (adr - rfr) / sddr
+
+    return cr, adr, sddr, sr
 
 
 def test_code():
@@ -63,7 +92,7 @@ def test_code():
     # note that during autograding his function will not be called.
     # Define input parameters
 
-    of = "./orders/orders2.csv"
+    of = "./orders/orders-short.csv"
     sv = 1000000
 
     # Process orders
@@ -73,14 +102,15 @@ def test_code():
     else:
         "warning, code did not return a DataFrame"
 
-    '''
     # Get portfolio stats
-    # Here we just fake the data.
-    # You should use your code from previous assignments.
-    start_date = dt.datetime(2008,1,1)
-    end_date = dt.datetime(2008,6,1)
-    cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = [0.2,0.01,0.02,1.5]
-    cum_ret_SPY, avg_daily_ret_SPY, std_daily_ret_SPY, sharpe_ratio_SPY = [0.2,0.01,0.02,1.5]
+    start_date = dt.datetime.date(portvals.index[0])
+    end_date = dt.datetime.date(portvals.index[-1])
+    cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = compute_portfolio_stats(portvals)
+
+    spy_vals = get_data(['SPY'], pd.date_range(start_date, end_date))
+    spy_vals = spy_vals[spy_vals.columns[0]]
+
+    cum_ret_SPY, avg_daily_ret_SPY, std_daily_ret_SPY, sharpe_ratio_SPY = compute_portfolio_stats(spy_vals)
 
     # Compare portfolio against $SPX
     print "Date Range: {} to {}".format(start_date, end_date)
@@ -98,7 +128,6 @@ def test_code():
     print "Average Daily Return of SPY : {}".format(avg_daily_ret_SPY)
     print
     print "Final Portfolio Value: {}".format(portvals[-1])
-    '''
 
 
 if __name__ == "__main__":
