105f1f0cadb5600bac63d509fed5e354974182b0
jrajamaki <jrajamaki@users.noreply.github.com>
2017-07-18 16:38:50 +0800

    Add testing for RTLearner

--- mc3_p1/testlearner.py
+++ mc3_p1/testlearner.py
@@ -5,46 +5,54 @@ Test a learner.  (c) 2015 Tucker Balch
 import numpy as np
 import math
 import LinRegLearner as lrl
+import RTLearner as rt
 import sys
 
-if __name__=="__main__":
+if __name__ == "__main__":
     if len(sys.argv) != 2:
         print "Usage: python testlearner.py <filename>"
         sys.exit(1)
-    inf = open(sys.argv[1])
-    data = np.array([map(float,s.strip().split(',')) for s in inf.readlines()])
+    f = open(sys.argv[1])
+    data = np.array([map(float, s.strip().split(',')) for s in f.readlines()])
 
     # compute how much of the data is training and testing
-    train_rows = math.floor(0.6* data.shape[0])
-    test_rows = data.shape[0] - train_rows
+    train_rows = int(math.floor(0.6 * data.shape[0]))
+    test_rows = int(data.shape[0] - train_rows)
 
     # separate out training and testing data
-    trainX = data[:train_rows,0:-1]
-    trainY = data[:train_rows,-1]
-    testX = data[train_rows:,0:-1]
-    testY = data[train_rows:,-1]
+    train_x = data[:train_rows, 0:-1]
+    train_y = data[:train_rows, -1]
+    test_x = data[train_rows:, 0:-1]
+    test_y = data[train_rows:, -1]
 
-    print testX.shape
-    print testY.shape
+    print test_x.shape
+    print test_y.shape
 
-    # create a learner and train it
-    learner = lrl.LinRegLearner(verbose = True) # create a LinRegLearner
-    learner.addEvidence(trainX, trainY) # train it
+    print '-- LINEAR REGRESSION --'
+    learner = lrl.LinRegLearner(verbose=True)
+    learner.addEvidence(train_x, train_y)
 
-    # evaluate in sample
-    predY = learner.query(trainX) # get the predictions
-    rmse = math.sqrt(((trainY - predY) ** 2).sum()/trainY.shape[0])
-    print
     print "In sample results"
+    pred_y = learner.query(train_x)  # get the predictions
+    rmse = math.sqrt(((train_y - pred_y) ** 2).sum() / train_y.shape[0])
     print "RMSE: ", rmse
-    c = np.corrcoef(predY, y=trainY)
-    print "corr: ", c[0,1]
+    c = np.corrcoef(pred_y, y=train_y)
+    print "corr: ", c[0, 1]
 
-    # evaluate out of sample
-    predY = learner.query(testX) # get the predictions
-    rmse = math.sqrt(((testY - predY) ** 2).sum()/testY.shape[0])
-    print
     print "Out of sample results"
+    pred_y = learner.query(test_x)  # get the predictions
+    rmse = math.sqrt(((test_y - pred_y) ** 2).sum() / test_y.shape[0])
     print "RMSE: ", rmse
-    c = np.corrcoef(predY, y=testY)
-    print "corr: ", c[0,1]
+    c = np.corrcoef(pred_y, y=test_y)
+    print "corr: ", c[0, 1]
+
+    print '-- RANDOM TREE --'
+    learner = rt.RTLearner(leaf_size=1, verbose=False)  # constructor
+    learner.addEvidence(train_x, train_y)  # training step
+
+    print "In sample results"
+    pred_y = learner.query(train_x)  # query
+    rmse = math.sqrt(((train_y - pred_y) ** 2).sum() / train_y.shape[0])
+    print "RMSE: ", rmse
+    c = np.corrcoef(pred_y, y=train_y)
+    print "corr: ", c[0, 1]
