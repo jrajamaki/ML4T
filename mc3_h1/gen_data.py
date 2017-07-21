65bfc6f06c047f7a4dab7f5d5ed54189588e997a
jrajamaki <jrajamaki@users.noreply.github.com>
2017-07-21 14:12:58 +0800

    Add correct data generation algorithms

--- mc3_h1/gen_data.py
+++ mc3_h1/gen_data.py
@@ -3,19 +3,23 @@ template for generating data to fool learners (c) 2016 Tucker Balch
 """
 
 import numpy as np
-import math
+
 
 # this function should return a dataset (X and Y) that will work
 # better for linear regresstion than random trees
-def best4LinReg():
-    X = np.random.normal(size = (100, 4)) 
-    Y = 0.1 * X[:,0] + 0.5 * np.sin(X[:,1]) + 0.1 * np.random.normal(size = 100)
+def best4LinReg(seed=5):
+    prng = np.random.RandomState(int(seed))
+    X = prng.normal(size=(1000, 10))
+    Y = X[:, 0]
     return X, Y
 
-def best4RT():
-    X = np.random.normal(size = (50, 2)) 
-    Y = 0.8 * X[:,0] + 5.0 * X[:,1] 
+
+def best4RT(seed=5):
+    prng = np.random.RandomState(int(seed))
+    X = prng.normal(size=(2, 10))
+    Y = (X ** 2).sum(axis=1)
     return X, Y
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     print "they call me Tim."
