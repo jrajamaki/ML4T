0c1fe954321cd1fe2614862c3afe76f1204b4fab
jrajamaki <jrajamaki@users.noreply.github.com>
2017-07-25 13:48:36 +0800

    Add Dyna optimisation capability

--- mc3_p2/QLearner.py
+++ mc3_p2/QLearner.py
@@ -25,7 +25,7 @@ class QLearner(object):
         @param gamma float [0, 1], the discount rate used in the update rule.
         @param rar float [0, 1], random action rate.
         @param radr float [0, 1], random action decay rate: rar = rar * radr.
-        @param dyna integer, conduct this number of dyna updates.
+        @param dyna integer, run this number of dyna optimisations.
         @param verbose boolean, if True, debugging statements are allowed.
         """
         self.num_states = num_states
@@ -38,66 +38,67 @@ class QLearner(object):
         self.verbose = verbose
 
         self.Q = np.zeros(shape=(num_states, num_actions))
+        self.dyna_model = np.empty((0, 4), dtype=int)
         self.state = 0
         self.action = 0
 
     def querysetstate(self, s):
         """
-        @summary: Update the state without updating the Q-table
+        @summary: Updates the state without updating the Q-table
         @param s: The new state
         @returns: The selected action
         """
         self.state = s
-        self.action = self._next_action()
-
-        if self.verbose:
-            print "s =", self.state, "a =", self.action
+        self.action = self._generate_next_action()
 
         return self.action
 
     def query(self, s_prime, r):
         """
-        @summary: Update the Q table and return an action
+        @summary: Updates learner status, runs optionally Dyna optimisation
         @param s_prime: The new state
         @param r: The reward in the new state
-        @returns: The selected action
+        @returns: The next action to take
         """
 
         # Q-learning algorithm steps:
         # 1) Update the Q table with new experience tuple <s,a,s_prime,r>
-        # 2) Decide whether to take random action or not, update rar
-        # 3) If yes return random action
-        # 4) If no, query Q table to find optimal action
+        # 2) Take a next action from new state either through
+        # look-up table Q or by random generation of random chance self.rar
+        # 3) Update rar by deteriorate rate of radr
         self._update_Q_table(self.state, self.action, s_prime, r)
 
-        self.state = s_prime
-        self.action = self._next_action()
-        self.rar *= self.radr
-
-        # Q-learning Dyna algorithm
-        # In addition hallucinate an experience:
+        # Optional Q-learning Dyna algorithm
         # 1) Choose random s and a
-        # 2) Query table T and determine new state prime_s according to probs
-        # 3) Query table R and determine reward r
+        # 2) Query transition table T and determine new state prime_s
+        # 3) Query reward table R and determine reward r
         # 4) Update Q table
         # Iterate over above algorithm
 
-        # Tip: Dyna hallucinates from experiences already had
-        # it is not necessary to create T or R tables
-        self._dyna()
+        # In this implementation each experience tuple is saved
+        # and data is chosen from experience tuple list,
+        # thus every time new experience tuple is added and only then
+        # Dyna optimisation is run
+        if self.dyna != 0:
+            new_exp_tuple = np.array([[self.state, self.action, s_prime, r]])
+            self.dyna_model = np.r_[self.dyna_model, new_exp_tuple]
+            self._run_dyna_optimisation()
 
-        if self.verbose:
-            print "s =", s_prime, "a =", self.action, "r =", r
+        # Update learner status
+        self.state = s_prime
+        self.action = self._generate_next_action()
+        self.rar *= self.radr
 
         return self.action
 
-    def _next_action(self):
+    def _generate_next_action(self):
         """
         @summary: Decides next action either through look up or randomly
         @returns: Action to take
         """
         probs = [self.rar, 1 - self.rar]
         random_action = np.random.choice([True, False], p=probs)
+
         if random_action:
             action = np.random.randint(self.num_actions)
         else:
@@ -109,32 +110,35 @@ class QLearner(object):
         """
         @summary: Updates lookup table Q
         """
-        a_prime = np.argmax(self.Q[s_prime, :])
+        if type(s_prime) is np.ndarray:
+            a_prime = np.argmax(self.Q[s_prime, :], axis=1)
+        else:
+            a_prime = np.argmax(self.Q[s_prime, :])
+
         new_Q_value = (1 - self.alpha) * self.Q[s, a] + \
                         self.alpha * (reward +
                                       self.gamma * self.Q[s_prime, a_prime])
-
         self.Q[s, a] = new_Q_value
 
-    def _dyna(self):
-        pass
-        '''
-        random_s = np.random.randint(self.num_states, size=self.dyna)
-        random_a = np.random.randint(self.num_actions, size=self.dyna)
+    def _run_dyna_optimisation(self):
+        """
+        @summary: Runs optional Dyna optimisation
+        """
 
-            for d in range(self.dyna):
-                # random initial state and action
-                random_s = np.random.randint(self.num_states)
-                random_a = np.random.randint(self.num_actions)
+        # From within all experience tuples randomly choose 
+        # 'self.dyna' amount by index
+        random_index = np.random.choice(self.dyna_model.shape[0],
+                                        size=self.dyna,
+                                        replace=True)
 
-                #
-                states, counts = np.unique(self.Q[random_s, :], return_counts=True)
-                for i in range(len(states)):
-                    gen_s = int(states[i])
-                    gen_r = self.Q[gen_s, random_a]
-                    gen_r *= counts[i] / counts.sum()
-                    self._update_Q_table(random_s, random_a, gen_s, gen_r)
-        '''
+        # Set dyna variables for optimisation
+        s = self.dyna_model[random_index, 0]
+        a = self.dyna_model[random_index, 1]
+        s_prime = self.dyna_model[random_index, 2]
+        r = self.dyna_model[random_index, 3]
+
+        self._update_Q_table(s, a, s_prime, r)
+
 
 if __name__ == "__main__":
     print "Remember Q from Star Trek? Well, this isn't him"
