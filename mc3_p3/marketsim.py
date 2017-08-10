745824c217f1814ed7afb460f570f259dc10e7c5
jrajamaki <jrajamaki@users.noreply.github.com>
2017-08-10 17:19:39 +0800

    Add helper functions

---
+++ mc3_p3/marketsim.py
@@ -0,0 +1,100 @@
+"""MC2-P1: Market simulator."""
+
+import pandas as pd
+import datetime as dt
+import util as ut
+
+
+# Leverage-function: borrowing money property
+# How much invested in market / liquidation value of account
+def check_leverage(new_pos):
+    leverage = (new_pos[:-1].abs().sum()) / (new_pos.sum())
+
+    if abs(leverage.values) > 2:
+        return False
+
+    return True
+
+
+def compute_portvals(orders_file="./orders/orders.csv", start_val=1000000):
+    # this is the function the autograder will call to test your code
+    # TODO: Your code here
+
+    # Get data
+    # FORMAT: Date, Symbol, Order(Buy/Sell), # of shares
+    orders = pd.read_csv(orders_file, index_col=[0])
+    orders = orders.sort_index()
+
+    # Determine stock symbols, start and end date from the 'orders'
+    ls_symbols = list(set(orders['Symbol'].values))
+    start_date = orders.index[0]
+    end_date = orders.index[-1]
+    dates = pd.date_range(start_date, end_date)
+
+    # Read prices to 'prices' dataframe, use adjusted close price
+    # Automatically adds SPY to ensure correct market dates
+    prices = ut.get_data(ls_symbols, dates)
+
+    # Create dataframe to follow daily positions
+    # At first no trades, thus every day only cash
+    position = pd.DataFrame(index=prices.index, columns=prices.columns)
+    position = position.fillna(0)
+    position['Cash'] = start_val
+
+    for order in orders.iterrows():
+        multiplier = 1
+        if order[1]['Order'] == 'SELL':
+            multiplier = -1
+
+        # Parsing
+        date = order[0]
+        stock = order[1]['Symbol']
+        amount = order[1]['Shares']
+        trade_val = multiplier * amount * prices.ix[date:, stock]
+
+        # Create new position
+        new_pos = position.ix[date].copy().to_frame()
+        new_pos.ix[stock] += trade_val
+        new_pos.ix['Cash'] -= trade_val
+
+        # Update new position
+        position.ix[date:, stock] += trade_val
+        position.ix[date:, 'Cash'] -= trade_val[date]
+
+    # Calculate daily portfolio value by summing 'position' dataframe's columns
+    port_val = position.sum(axis=1).to_frame()
+
+    return port_val
+
+
+def test_code():
+    # this is a helper function you can use to test your code
+    # note that during autograding his function will not be called.
+    # Define input parameters
+
+    of = "./testcases2016/orders-12-modified.csv"
+    sv = 1000000
+
+    # Process orders
+    portvals = compute_portvals(orders_file=of, start_val=sv)
+    if isinstance(portvals, pd.DataFrame):
+        portvals = portvals[portvals.columns[0]]  # just get the first column
+    else:
+        "warning, code did not return a DataFrame"
+
+    # Get portfolio stats
+    start_date = dt.datetime.date(portvals.index[0])
+    end_date = dt.datetime.date(portvals.index[-1])
+    cr, adr, stddr, sr, fv = ut.compute_portfolio_stats(portvals)
+
+    # Portfolio statistics
+    print "Date Range: {} to {}".format(start_date, end_date)
+    print "Sharpe Ratio of Fund: {}".format(sr)
+    print "Cumulative Return of Fund: {}".format(cr)
+    print "Standard Deviation of Fund: {}".format(stddr)
+    print "Average Daily Return of Fund: {}".format(adr)
+    print "Final Portfolio Value: {}".format(fv)
+
+
+if __name__ == "__main__":
+    test_code()
--- mc3_p3/util.py
+++ mc3_p3/util.py
@@ -3,12 +3,19 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import datetime as dt
+import math
+import csv
+import numpy as np
+import marketsim
+
 
 def symbol_to_path(symbol, base_dir=os.path.join("..", "data")):
     """Return CSV file path given ticker symbol."""
     return os.path.join(base_dir, "{}.csv".format(str(symbol)))
 
-def get_data(symbols, dates, addSPY=True, colname = 'Adj Close'):
+
+def get_data(symbols, dates, addSPY=True, colname='Adj Close'):
     """Read stock data (adjusted close) for given symbols from CSV files."""
     df = pd.DataFrame(index=dates)
     if addSPY and 'SPY' not in symbols:  # add SPY for reference, if absent
@@ -16,7 +23,8 @@ def get_data(symbols, dates, addSPY=True, colname = 'Adj Close'):
 
     for symbol in symbols:
         df_temp = pd.read_csv(symbol_to_path(symbol), index_col='Date',
-                parse_dates=True, usecols=['Date', colname], na_values=['nan'])
+                              parse_dates=True, usecols=['Date', colname],
+                              na_values=['nan'])
         df_temp = df_temp.rename(columns={colname: symbol})
         df = df.join(df_temp)
         if symbol == 'SPY':  # drop dates SPY did not trade
@@ -24,9 +32,152 @@ def get_data(symbols, dates, addSPY=True, colname = 'Adj Close'):
 
     return df
 
+
+def compute_portfolio_stats(port_val, rfr=0.0, sf=252.0):
+
+    # Calculate daily returns
+    daily_returns = port_val / port_val.shift(1) - 1
+    daily_returns = daily_returns[1:]
+    # Get portfolio statistics (note: std_daily_ret = volatility)
+    cr = port_val.ix[-1] / port_val.ix[0] - 1
+    adr = daily_returns.mean()
+    sddr = daily_returns.std()
+    sr = math.sqrt(sf) * (adr - rfr) / sddr
+
+    return cr * 100, adr * 100, sddr, sr, port_val[-1]
+
+
+def draw_charts(portfolios, *orders):
+    plt.style.use('ggplot')
+    fig, ax = plt.subplots(2, 1)
+    colors = ['black', 'blue', 'green']
+    ax[0].set_prop_cycle('color', colors)
+
+    data = []
+    columns = []
+
+    for portfolio in portfolios:
+        data.append(compute_portfolio_stats(portfolio))
+        columns.append(portfolio.name)
+        portfolio.divide(portfolio.ix[0]).plot(ax=ax[0], legend=True)
+
+    if orders:
+        orders = orders[0]
+        pos = 0
+        for index, order in orders.iterrows():
+            pos += int(order[0])
+            if pos != 0:
+                color = 'green' if pos == 200 else 'red'
+                ax[0].axvline(index, color=color)
+
+    data = np.transpose(np.asarray(data))
+    rows = ("Cumulative return (%)", "Average daily return (%)",
+            "Standard deviation", "Sharpe ratio", "Final value ($)")
+    ax[1].table(cellText=data, colLabels=columns, rowLabels=rows,
+                cellLoc='center', loc='center')
+    ax[1].axis('tight')
+    ax[1].axis('off')
+
+    plt.show()
+
+
+def write_orders_to_csv(orders, filename):
+    syms = orders.columns
+
+    # FORMAT: Date, Symbol, Order(Buy/Sell), # of shares
+    with open(filename, 'wb') as file:
+        writer = csv.writer(file)
+        writer.writerow(('Date', 'Symbol', 'Order', 'Shares'))
+        for index, row in orders.iterrows():
+            date = index.strftime('%Y-%m-%d')
+
+            for i in range(len(syms)):
+                amount = abs(row[i])
+                if amount == 0:
+                    continue
+
+                symbol = syms[i]
+                if row[i] < 0:
+                    order_type = 'SELL'
+                else:
+                    order_type = 'BUY'
+
+                writer.writerow((date, symbol, order_type, amount))
+
+
+def print_full(df):
+    pd.set_option('display.max_rows', len(df))
+    print(df)
+    pd.reset_option('display.max_rows')
+
+
+def create_benchmark(sym, amount, initial_cash, start_date, end_date):
+    # get data
+    dates = pd.date_range(start_date, end_date)
+    prices_all = get_data([sym], dates)  # automatically adds SPY
+    prices = prices_all[[sym]]  # only portfolio symbols
+
+    output = './orders/benchmark.csv'
+    with open(output, 'wb') as file:
+        writer = csv.writer(file)
+        writer.writerow(('Date', 'Symbol', 'Order', 'Shares'))
+        start = prices.index[0]
+        end = prices.index[-1]
+        writer.writerow((start, sym, 'BUY', '200'))
+        writer.writerow((end, sym, 'SELL', '200'))
+
+    benchmark = marketsim.compute_portvals(orders_file=output,
+                                           start_val=initial_cash)
+    benchmark = benchmark[benchmark.columns[0]]
+    benchmark.rename('200 of ' + sym, inplace=True)
+    return benchmark
+
+
+def standardise_timeperiod(portfolio, start_date, end_date, initial_cash):
+    dates = pd.date_range(start_date, end_date)  # real analysis period
+    portfolio = portfolio.reindex(dates)
+    if np.isnan(portfolio[0]):  # no trade on first day
+        portfolio[0] = initial_cash
+
+    portfolio.ffill(inplace=True)
+    return portfolio
+
+'''
+def print_results(portfolio):
+    # Get portfolio stats
+    start_date = dt.datetime.date(portfolio.index[0])
+    end_date = dt.datetime.date(portfolio.index[-1])
+    cr, adr, stddr, sr, fv = compute_portfolio_stats(portfolio)
+
+    # Portfolio statistics
+    print portfolio.name, "results"
+    print "Date Range: {} to {}".format(start_date, end_date)
+    print "Sharpe Ratio: {:2.4}".format(sr)
+    print "Cumulative Return: {:2.4}%".format(cr * 100)
+    print "Standard Deviation: {:.4}".format(stddr)
+    print "Average Daily Return: {:.2}%".format(adr * 100)
+    print "Final Value: {}".format(fv)
+
+
 def plot_data(df, title="Stock prices", xlabel="Date", ylabel="Price"):
     """Plot stock prices with a custom title and meaningful axis labels."""
     ax = df.plot(title=title, fontsize=12)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
-    plt.show()
+    plt.show()    
+
+
+def save_benchmark_data(sym, start_date, end_date, output='benchmark.csv'):
+    # get data
+    dates = pd.date_range(start_date, end_date)
+    prices_all = get_data([sym], dates)  # automatically adds SPY
+    prices = prices_all[[sym]]  # only portfolio symbols
+
+    with open(output, 'wb') as file:
+        writer = csv.writer(file)
+        writer.writerow(('Date', 'Symbol', 'Order', 'Shares'))
+        start = prices.index[0]
+        end = prices.index[-1]
+        writer.writerow((start, sym, 'BUY', '200'))
+        writer.writerow((end, sym, 'SELL', '200'))
+'''
