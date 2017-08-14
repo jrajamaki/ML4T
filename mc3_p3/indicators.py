0097b2bccf53c7dce7f952217ab17d2294f56f93
jrajamaki <jrajamaki@users.noreply.github.com>
2017-08-14 10:45:38 +0800

    Fix normalisation bug Add comments

--- mc3_p3/indicators.py
+++ mc3_p3/indicators.py
@@ -17,33 +17,53 @@ import seaborn as sns
 import numpy as np
 
 
-def run_analysis(sym='AAPL',
-                 start_date=dt.datetime(2008, 1, 1),
-                 end_date=dt.datetime(2009, 12, 31),
-                 lookback_pers=10,
-                 draw_charts=False,
-                 normalise=True):
+def standard_score(df):
+    """
+    @summary: Normalises Pandas dataframe.
+    @param df: Dataframe to be normalised.
+    @return: Normalised dataframe.
+    """
+    return (df - df.mean()) / df.std()
 
-    per = lookback_pers  # period to look back
+
+def run_analysis(sym, start_date, end_date, look_back=21,
+                 normalise=False, draw_charts=False):
+    """
+    @summary: Normalises Pandas dataframe.
+    @param sym: Stock symbol to make the analysis on.
+    @param start_date: The starting date to commence analysis.
+    @param end_date: The ending date to stop analysis.
+    @param look_back: Number of days to the past to calculate indicators.
+    @param normalise: If True normalises indicators in resulting dataframe.
+    @param draw_charts: If True draws charts of indicators.
+    @return: Pandas dataframe of stock price and indicator data.
+    """
+
+    # to simplify code
+    per = look_back
+    sym = [sym]
 
     # get data
     dates = pd.date_range(start_date, end_date)
-    prices_all = ut.get_data([sym], dates)  # automatically adds SPY
-    prices = prices_all[[sym]]  # only portfolio symbols
+    prices_all = ut.get_data(sym, dates)  # automatically adds SPY
+    prices = prices_all[sym]  # only portfolio symbols
     n_prices = prices.divide(prices.ix[0])  # normalise
 
-    # Calculate indicators
+    # Smooth moving average / price index
     smap = n_prices.copy()
     smap['SMA'] = n_prices.rolling(per, min_periods=per).mean()
     smap['SMA / P'] = prices.rolling(per, min_periods=per).mean() / prices
 
+    # Bolling bands index
     bb = n_prices.copy()
     bb['SMA'] = n_prices.rolling(per, min_periods=per).mean()
     bb['MSD'] = n_prices.rolling(per, min_periods=per).std()  # moving std
     bb['Upper BB'] = bb['SMA'] + 2 * bb['MSD']
     bb['Lower BB'] = bb['SMA'] - 2 * bb['MSD']
-    bb['BBI'] = (bb.ix[:, 0] - bb['Lower BB']) / (bb['Upper BB'] - bb['Lower BB'])
+    bb['BBI'] = (bb.ix[:, 0] - bb['Lower BB']) / (bb['Upper BB'] -
+                                                  bb['Lower BB'])
 
+    # Relative strenght index
     rsi = n_prices.copy()
     rsi['Rets'] = rsi.diff(periods=1)
     rsi['Gain'] = rsi['Rets'][rsi['Rets'] > 0].rolling(per).sum()
@@ -53,11 +73,11 @@ def run_analysis(sym='AAPL',
     rsi['RS'] = rsi['Avg gain'] / rsi['Avg loss']
     rsi['RSI'] = 100 - (100 / (1 + rsi['RS']))
 
+    # Momentum index
     momentum = n_prices.copy()
     momentum['Momentum'] = momentum.divide(momentum.shift(per)) - 1
 
     if normalise:
-        standard_score = lambda x: (x - x.mean()) / x.std()
         smap['SMA / P'] = standard_score(smap['SMA / P'])
         bb['BBI'] = standard_score(bb['BBI'])
         rsi['RSI'] = standard_score(rsi['RSI'])
@@ -71,10 +91,18 @@ def run_analysis(sym='AAPL',
     plt.close('all')
     if draw_charts:
         f, ax = plt.subplots(2, 2, sharex='col', sharey='row')
+        '''
         smap.plot(ax=ax[0, 0], legend=True, title='SMA / P')
         bb.plot(ax=ax[0, 1], legend=True, title='BB Index')
         rsi.plot(ax=ax[1, 0], legend=True, title='RS Index')
         momentum.plot(ax=ax[1, 1], legend=True, title='Momentum')
+        '''
+        smap[sym + ['SMA / P']].plot(ax=ax[0, 0], legend=True, title='SMA / P')
+        bb[sym + ['BBI']].plot(ax=ax[0, 1], legend=True, title='BB Index')
+        rsi[sym + ['RSI']].plot(ax=ax[1, 0], legend=True, title='RS Index')
+        momentum[sym + ['Momentum']].plot(ax=ax[1, 1], legend=True,
+                                          title='Momentum')
+
         f.autofmt_xdate()
 
         f, ax = plt.subplots()
@@ -82,6 +110,7 @@ def run_analysis(sym='AAPL',
         sns.heatmap(corr, mask=np.zeros_like(corr, dtype=np.bool),
                     cmap=sns.diverging_palette(220, 10, as_cmap=True),
                     square=True, ax=ax)
+
         plt.show()
 
     return key_data
@@ -91,5 +120,6 @@ if __name__ == '__main__':
     run_analysis(sym='AAPL',
                  start_date=dt.datetime(2008, 1, 1),
                  end_date=dt.datetime(2009, 12, 31),
-                 lookback_pers=10,
+                 look_back=21,
+                 normalise=True,
                  draw_charts=True)
